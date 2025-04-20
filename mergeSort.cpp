ce7b4651dcea2c6479c87bb7842db71237209ed4
RanggaAlfarizzy <rangga.alfarizzy@gmail.com>
2025-04-20 14:41:17 +0800

    Memasukkan Jumlah dan isi elemen array dan mengulangi hingga jumlah elemen sesuai

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -3,3 +3,15 @@ using namespace std;
 
 int arr[20], B[20]; // Array Utama dan Array bantu
 int n;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan Panjang element array : ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        cout << "\nMaksimal panjang array adalah 20" << endl;
+    }
+}
